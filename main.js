86ee7a88f6b4687de585b0db6011f7f573e59e53
yanivin08 <36818136+yanivin08@users.noreply.github.com>
2018-08-16 18:29:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,16 @@ let	div = document.getElementsByTagName("div" )[0],
 	bgCtx = bgCanvas.getContext("2d"),
 	canvas = document.getElementById("game"),
 	ctx = canvas.getContext("2d");
-
+//background canvas
 bgCanvas.width = div.clientWidth;
 bgCanvas.height = div.clientHeight;
+//game canvas
 canvas.width = div.clientWidth;
 canvas.height = div.clientHeight;
-
+//get the boundbox for the mouse x and y;
 let gameBox = canvas.getBoundingClientRect();
 
-let mouseX, mouseY, spaceShips, shotGuns, createBg, ships = [], bullets = [];
+let mouseX, mouseY, spaceShips, shotGuns, createBg, enemy = [], bullets = [];
 
 let spaceImg = new Image;
 spaceImg.src = "space.png";
@@ -20,28 +21,31 @@ let shootImg = new Image;
 shootImg.src = "shot.png";
 
 let shotY = (canvas.height-(canvas.height*.15))-shootImg.height*.07/2;
-
+//this is where we put the moving background
 class background{
 	constructor(){
+		//initialized background
 		this.speed = 1;
 		this.x = 0;
 		this.bg = new Image;
 		this.bg.src = "bgstar.png"
 	}
 	drawBackground(){
-		//bgCtx.clearRect(0,0,bgCanvas.width,bgCanvas.height);
 		this.x += this.speed
+		//add the first layer of the background
 		bgCtx.drawImage(this.bg,0,this.x,this.bg.width*.775,this.bg.height*.775)
+		//then add the second layer at the top of the first background
 		bgCtx.drawImage(this.bg,0,this.x-bgCanvas.height,this.bg.width*.775,this.bg.height*.775)
-		
+		//loop once the first layer reach the end of the canvas.
 		if(this.x >= bgCanvas.height){
 			this.x = 0;
 		}
 	}
 }
-
+//draw the spacecraft
 class spacecraft{
 	constructor(x,y,dX,dY){
+		//initialized spacecraft
 		this.x = x;
 		this.y = y;
 		this.dx = dX;
@@ -50,13 +54,8 @@ class spacecraft{
 		this.trueY = (canvas.width/2 - ((dY - gameBox.y) + canvas.width/2)) + canvas.width/2;
 		this.deg = (Math.atan2(this.trueY,this.trueX)) * 180/Math.PI;
 	}
-	drawLine(){
-		ctx.beginPath();
-		ctx.moveTo(this.x,this.y);
-		ctx.lineTo(this.dx-gameBox.left,this.dy-gameBox.top);
-		ctx.stroke();
-	}
 	drawImage(){
+		//draw the spacecraft
 		ctx.clearRect(0,0,canvas.width,canvas.height);
 		if(((this.dx-gameBox.x)-spaceImg.width*.15/2)>10 &&((this.dx-gameBox.x)-spaceImg.width*.15/2)<420){
 			ctx.drawImage(spaceImg,(this.dx-gameBox.x)-spaceImg.width*.15/2,(canvas.height-(canvas.height*.10))-spaceImg.height*.15/2,spaceImg.width*.15,spaceImg.height*.15);
@@ -66,7 +65,6 @@ class spacecraft{
 			ctx.drawImage(spaceImg,420,(canvas.height-(canvas.height*.10))-spaceImg.height*.15/2,spaceImg.width*.15,spaceImg.height*.15);
 		}
 	}
-	
 }
 
 class shootGun{
@@ -90,27 +88,30 @@ class shootGun{
 	}
 }
 
+//function for animation
 function start(){
+	//draw the background
 	createBg.drawBackground();
-	ships.forEach(x => x.drawImage());
+	//add the spaceships
+	spaceShips = new spacecraft(canvas.width/2,canvas.height/2,mouseX,mouseY);
+	spaceShips.drawImage();
+	//draw the bullets if trigger
 	bullets.forEach(x => x.triggerGun());
+	//loop
 	requestAnimationFrame(start);
 }
 
+//this is where we add bullets on mouse click
 window.onclick = function(e){
-	//spaceShips.shootGun();
 	shotGuns = new shootGun(e.clientX,e.clientY,shotY);
 	bullets.push(shotGuns);
 }
 
+//get the x and y coordinates on mouse move
 window.onmousemove = function(e){
 	mouseX = e.clientX;
 	mouseY = e.clientY;
-	spaceShips = new spacecraft(canvas.width/2,canvas.height/2,mouseX,mouseY);
-	ships.push(spaceShips);
 }
 
 createBg = new background();
-
-
 start();
