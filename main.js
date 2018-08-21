181b6cb2a5e4b5af45429b9cd44453c1d87ce416
yanivin08 <36818136+yanivin08@users.noreply.github.com>
2018-08-21 21:43:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let	div = document.getElementsByTagName("div" )[0],
 	bgCanvas = document.getElementById("background"),
 	bgCtx = bgCanvas.getContext("2d"),
@@ -10,22 +9,18 @@ bgCanvas.height = div.clientHeight;
 //game canvas
 canvas.width = div.clientWidth;
 canvas.height = div.clientHeight;
-//get the boundbox for the mouse x and y;
+
 let gameBox = canvas.getBoundingClientRect();
+let postX = canvas.width/2,
+	postY = canvas.height - canvas.height * .08,
+	gameArr = [], count = 20;
+	
+let keyPress = {a:0,w:0,d:0,s:0}
 
-let mouseX, mouseY, spaceShips, shotGuns, createBg, enemy = [], bullets = [];
-
-let spaceImg = new Image;
-spaceImg.src = "space.png";
-let shootImg = new Image;
-shootImg.src = "shot.png";
-
-let shotY = (canvas.height-(canvas.height*.15))-shootImg.height*.07/2;
-//this is where we put the moving background
 class background{
 	constructor(){
 		//initialized background
-		this.speed = 1;
+		this.speed = 2;
 		this.x = 0;
 		this.bg = new Image;
 		this.bg.src = "bgstar.png"
@@ -42,75 +37,111 @@ class background{
 		}
 	}
 }
-//draw the spacecraft
-class spacecraft{
-	constructor(x,y,dX,dY){
-		//initialized spacecraft
+
+class games{
+	constructor(x,y,left,right){
 		this.x = x;
 		this.y = y;
-		this.dx = dX;
-		this.dy = dY;
-		this.trueX = (dX - gameBox.x) - canvas.width/2;
-		this.trueY = (canvas.width/2 - ((dY - gameBox.y) + canvas.width/2)) + canvas.width/2;
-		this.deg = (Math.atan2(this.trueY,this.trueX)) * 180/Math.PI;
+		this.spaceRad = 50;
+		this.gunX = x;
+		this.gunY = y;
+		this.gunLeft = left;
+		this.gunRight = right;
+		this.enemyY = -100;
+		this.enemyX = Math.floor((Math.random() * canvas.width - (canvas.width*15)) + canvas.width * .05);
+		this.spaceImg = new Image;
+		this.spaceImg.src = "space.png";
+		this.shootImg = new Image;
+		this.shootImg.src = "shot.png";
 	}
-	drawImage(){
-		//draw the spacecraft
-		ctx.clearRect(0,0,canvas.width,canvas.height);
-		if(((this.dx-gameBox.x)-spaceImg.width*.15/2)>10 &&((this.dx-gameBox.x)-spaceImg.width*.15/2)<420){
-			ctx.drawImage(spaceImg,(this.dx-gameBox.x)-spaceImg.width*.15/2,(canvas.height-(canvas.height*.10))-spaceImg.height*.15/2,spaceImg.width*.15,spaceImg.height*.15);
-		}else if(((this.dx-gameBox.x)-spaceImg.width*.15/2)<10){
-			ctx.drawImage(spaceImg,10,(canvas.height-(canvas.height*.10))-spaceImg.height*.15/2,spaceImg.width*.15,spaceImg.height*.15);
-		}else if(((this.dx-gameBox.x)-spaceImg.width*.15/2)>420){
-			ctx.drawImage(spaceImg,420,(canvas.height-(canvas.height*.10))-spaceImg.height*.15/2,spaceImg.width*.15,spaceImg.height*.15);
+	drawShip(){
+		//change the x and y coordination on keypress
+		if(keyPress.a == 3){this.x -= keyPress.a}
+		else if(keyPress.d == 3){this.x += keyPress.d}
+		else if(keyPress.w == 3){this.y -= keyPress.w}
+		else if(keyPress.s == 3){this.y += keyPress.s}
+		//draw canvas with the new coordinates
+		if(this.x-this.spaceImg.width*.12/2 > canvas.width*.03 && this.x-this.spaceImg.width*.12/2 < canvas.width - (canvas.width*.15) &&
+			this.y-this.spaceImg.height*.12/2 > canvas.height*.03 && this.y-this.spaceImg.height*.12/2 < canvas.height - (canvas.height*.15)){
+			ctx.beginPath();
+			ctx.arc(this.x,this.y,this.spaceRad,0,2*Math.PI);
+			ctx.stroke();
+			ctx.closePath();
+			
+			ctx.drawImage(this.spaceImg,this.x-this.spaceImg.width*.12/2,this.y-this.spaceImg.height*.12/2,this.spaceImg.width*.12,this.spaceImg.height*.12);
+		}else if(this.x-this.spaceImg.width*.12/2 < canvas.width*.03){
+			this.x += keyPress.a
+			ctx.drawImage(this.spaceImg,canvas.width*.03,this.y-this.spaceImg.height*.12/2,this.spaceImg.width*.12,this.spaceImg.height*.12);
+		}else if(this.x-this.spaceImg.width*.12/2 > canvas.width - (canvas.width*.15)){
+			this.x -= keyPress.d
+			ctx.drawImage(this.spaceImg,canvas.width - (canvas.width*.15),this.y-this.spaceImg.height*.12/2,this.spaceImg.width*.12,this.spaceImg.height*.12);
+		}else if(this.y-this.spaceImg.height*.12/2 < canvas.height*.03){
+			this.y += keyPress.w
+			ctx.drawImage(this.spaceImg,this.x-this.spaceImg.width*.12/2,canvas.height*.03,this.spaceImg.width*.12,this.spaceImg.height*.12);
+		}else if(this.y-this.spaceImg.height*.12/2 > canvas.height - (canvas.height*.15)){
+			this.y -= keyPress.s
+			ctx.drawImage(this.spaceImg,this.x-this.spaceImg.width*.12/2,canvas.height - (canvas.height*.15),this.spaceImg.width*.12,this.spaceImg.height*.12);
 		}
+		//then get the new coordinates and assign to the new set of array
+		postX = this.x;
+		postY = this.y;
+	}
+	triggerShoot(){
+		this.gunY -= 8;
+		ctx.drawImage(this.shootImg,this.gunX-this.gunLeft,this.gunY,this.shootImg.width*.05,this.shootImg.height*.05);
+		ctx.drawImage(this.shootImg,this.gunX+this.gunRight,this.gunY,this.shootImg.width*.05,this.shootImg.height*.05);
+	}
+	getEnemy(){
+
+		this.enemyY += 5;
+		
+		ctx.beginPath();
+		ctx.arc(this.enemyX,this.enemyY,50,0,2*Math.PI);
+		ctx.fill();
+		ctx.stroke();
+		ctx.closePath();
 	}
 }
 
-class shootGun{
-	constructor(dx,dy,speed){
-		this.dx = dx;
-		this.dy = dy;
-		this.speed = speed
-	}
-	triggerGun(){
-		this.speed -= 8;
-		if(((this.dx-gameBox.x)-spaceImg.width*.10/2)>10 &&((this.dx-gameBox.x)-spaceImg.width*.10/2)<420){
-			ctx.drawImage(shootImg,((this.dx-gameBox.x)-shootImg.width*.07/2)+35,this.speed,shootImg.width*.07,shootImg.height*.07)
-			ctx.drawImage(shootImg,((this.dx-gameBox.x)-shootImg.width*.07/2)-35,this.speed,shootImg.width*.07,shootImg.height*.07)
-		}else if(((this.dx-gameBox.x)-spaceImg.width*.15/2)<10){
-			ctx.drawImage(shootImg,80,this.speed,shootImg.width*.07,shootImg.height*.07)
-			ctx.drawImage(shootImg,10,this.speed,shootImg.width*.07,shootImg.height*.07)
-		}else if(((this.dx-gameBox.x)-spaceImg.width*.15/2)>420){
-			ctx.drawImage(shootImg,485,this.speed,shootImg.width*.07,shootImg.height*.07)
-			ctx.drawImage(shootImg,415,this.speed,shootImg.width*.07,shootImg.height*.07)
-		}
-	}
-}
 
-//function for animation
 function start(){
-	//draw the background
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	//game.drawShip();
+	count -= 1;
+	gameArr.forEach(x => x.drawShip());
+	if(count == 0){
+		//console.log(gameArr);
+		game = new games(postX,postY,35,25);
+		gameArr.push(game);
+		game = new games(postX,postY,35,25);
+		gameArr.push(game);
+		
+		count = 20;
+	}
+	
+	gameArr.forEach(x => x.drawShip());
+	gameArr.forEach(x => x.triggerShoot());
+	gameArr.forEach(x => x.getEnemy());
+	gameArr = gameArr.filter(x => x.gunY > 0);
+	//gameArr = gameArr.filter(x =>)
 	createBg.drawBackground();
-	//add the spaceships
-	spaceShips = new spacecraft(canvas.width/2,canvas.height/2,mouseX,mouseY);
-	spaceShips.drawImage();
-	//draw the bullets if trigger
-	bullets.forEach(x => x.triggerGun());
-	//loop
 	requestAnimationFrame(start);
 }
 
-//this is where we add bullets on mouse click
-window.onclick = function(e){
-	shotGuns = new shootGun(e.clientX,e.clientY,shotY);
-	bullets.push(shotGuns);
+window.onkeydown = function(e){
+	if(e.key == 'a' || e.key == 'ArrowLeft'){
+		keyPress.a = 3
+	}else if(e.key == 'd' || e.key == 'ArrowRight'){
+		keyPress.d = 3
+	}else if(e.key == 'w' || e.key == 'ArrowUp'){
+		keyPress.w = 3
+	}else if(e.key == 's' || e.key == 'ArrowDown'){
+		keyPress.s = 3
+	}
 }
 
-//get the x and y coordinates on mouse move
-window.onmousemove = function(e){
-	mouseX = e.clientX;
-	mouseY = e.clientY;
+window.onkeyup = function(){
+	keyPress = {a:0,s:0,d:0,w:0};
 }
 
 createBg = new background();
